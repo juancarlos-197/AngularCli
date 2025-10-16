e31c8c1cfa76b785c0e3e4a20c2d89ad055b6094
juancarlos-197 <juancarlos-197@unimayor.edu.co>
2025-10-16 05:57:55 -0500

    Get started with Tailwind CSS Tailwind CSS works by scanning all of your HTML files, JavaScript components, and any other templates for class names, generating the corresponding styles and then writing them to a static CSS file.

--- src/app/interceptor/error-handler.interceptor.ts
+++ src/app/interceptor/error-handler.interceptor.ts
@@ -2,7 +2,15 @@ import { HttpErrorResponse, HttpInterceptorFn } from '@angular/common/http';
 import { catchError, throwError } from 'rxjs';
 
 export const errorHandlerInterceptor: HttpInterceptorFn = (req, next) => {
-  return next(req).pipe(catchError((error: HttpErrorResponse)=>{
+ 
+  const started = Date.now();
+  console.log(`Petición saliente: ${req.method} ${req.url}`);
+
+  return next(req).pipe(
+    catchError((error: HttpErrorResponse)=>{
+     const elapsed = Date.now() - started;
+      console.log(`Petición completada: ${req.method} ${req.url} en ${elapsed} ms`);
+
       let errorMessage ="";
       if (error.error instanceof ErrorEvent) {
         errorMessage = `Error: ${error.message}`;
